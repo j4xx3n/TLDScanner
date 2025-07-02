d78444dcf00463690f27b79f3c87e6f4a1303f10
j4xx3n <105862349+j4xx3n@users.noreply.github.com>
2025-07-02 15:58:09 -0500

    Update tldscanner.sh

--- tldscanner.sh
+++ tldscanner.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
 
-run=$(python3 script/mainOrg.py -d cat.com)
+# Default values
+directory=""
+output=""
+
+# Parse options
+while getopts "d:o:" opt; do
+  case $opt in
+    d)
+      directory="$OPTARG"
+      ;;
+    o)
+      output="$OPTARG"
+      ;;
+    \?)
+      echo "Usage: $0 -d <directory> -o <output_file>"
+      exit 1
+      ;;
+  esac
+done
+
+
+run=$(python3 script/mainOrg.py -d "$directory")
 domain=$(echo $run | cut -d ' ' -f 1 | cut -d '.' -f 1)
 org=$(echo $run | cut -d ':' -f 2)
 echo Testing: $org
