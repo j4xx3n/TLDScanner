0a5cc3ea50de37d425a7987c735889213a4d6b1d
j4xx3n <105862349+j4xx3n@users.noreply.github.com>
2025-07-02 18:49:02 -0600

    Update tldscanner.sh

--- tldscanner.sh
+++ tldscanner.sh
@@ -5,7 +5,7 @@ directory=""
 output=""
 
 # Parse options
-while getopts "d:o:" opt; do
+while getopts "d:o:h" opt; do
   case $opt in
     d)
       directory="$OPTARG"
@@ -13,17 +13,30 @@ while getopts "d:o:" opt; do
     o)
       output="$OPTARG"
       ;;
+    h)
+      echo "Usage: $0 -d <directory> [-o <output_file>] [-h]"
+      echo
+      echo "Options:"
+      echo "  -d <directory>     Directory to scan"
+      echo "  -o <output_file>   Optional output file"
+      echo "  -h                 Show this help message"
+      exit 0
+      ;;
     \?)
-      echo "Usage: $0 -d <directory> -o <output_file>"
+      echo "Invalid option: -$OPTARG" >&2
+      echo "Use -h for help"
       exit 1
       ;;
   esac
 done
-
 
 run=$(python3 script/mainOrg.py -d "$directory")
 domain=$(echo $run | cut -d ' ' -f 1 | cut -d '.' -f 1)
 org=$(echo $run | cut -d ':' -f 2)
 echo Testing: $org
 
-python3 script/search.py -d "$domain" -o "$org"
+if [[ -n "$output" ]]; then
+    python3 script/search.py -d "$domain" -o "$org" > $output
+else:
+    python3 script/search.py -d "$domain" -o "$org"
+fi
